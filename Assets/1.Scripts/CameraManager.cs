c3513b4411e9cb5020a49dbf11233aa62a41258e
Lopeca <3167ls3@naver.com>
2025-05-20 12:24:01 +0900

    feat : 플레이어 구체 이동 구현
    보는 방향으로 구르는 구체

---
+++ Assets/1.Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameraState
+{
+    P1,
+    P3,
+    OnEvent
+}
+public class CameraManager : MonoBehaviour
+{
+    private CameraState state;
+
+    [SerializeField]
+    private Transform target;
+    
+    private Vector3 targetPosition;
+    private const float SmoothSpeed = 10f;
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * SmoothSpeed);
+
+    }
+}
---
+++ Assets/1.Scripts/Utility/AngleCalculator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AngleCalculator 
+{
+    public static float NormalizeAngle(float angle)
+    {
+        angle = angle % 360;
+        if (angle < 0) angle += 360;
+        return angle;
+    }
+}
