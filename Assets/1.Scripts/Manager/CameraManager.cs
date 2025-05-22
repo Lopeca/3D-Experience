387d91603b13c7192e7a043d774fcfe3b8f69b15
Lopeca <3167ls3@naver.com>
2025-05-22 20:39:38 +0900

    feat, fix : 카메라 3인칭 기능 추가, 떨림 현상 수정

--- Assets/1.Scripts/Manager/CameraManager.cs
+++ Assets/1.Scripts/Manager/CameraManager.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public enum CameraState
 {
     P1,
-    P3,
-    OnEvent
+    P3
 }
 public class CameraManager : MonoBehaviour
 {
     private CameraState state;
 
     [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
     private Transform target;
     
     private Vector3 targetPosition;
+    private readonly Vector3 thirdPerspectivePos = new Vector3(0, 0.2f, -5);
     private const float SmoothSpeed = 100f;
-
+    private const float ToggleSpeed = 10f;
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * SmoothSpeed);
-
+            transform.position = target.position;
+            
+            if (state == CameraState.P1)
+            {
+                mainCamera.transform.localPosition = Vector3.Lerp(mainCamera.transform.localPosition, Vector3.zero, Time.deltaTime * ToggleSpeed);
+            }
+            else
+            {
+                mainCamera.transform.localPosition = Vector3.Lerp(mainCamera.transform.localPosition, thirdPerspectivePos, Time.deltaTime * ToggleSpeed);
+            }
     }
+
+    public void ToggleState()
+    {
+        state = state == CameraState.P1 ? CameraState.P3 : CameraState.P1;
+        
+    }
+
 }
